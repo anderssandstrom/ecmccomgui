c3a60d970cc379d21d216b939cc57b1ca80da29a
Anders Sandström <anders.sandstrom@esss.se>
2020-12-09 12:52:21 +0100

    Add soem options and printouts to pyDataManip/histCaMonitor.py

--- pyDataManip/histCaMonitor.py
+++ pyDataManip/histCaMonitor.py
@@ -9,30 +9,35 @@ from caPVArrayLib import caPVArray
 from caMonitorArrayParserLib import caMonitorArrayParser 
 
 def printOutHelp():
-  print "python plotCaMonitor.py [<filename>]"
-  print "example: python plotCaMonitor.py xx.txt"
-  print "example stdin: cat data.log | grep -E 'thread|CPU' | python plotCaMonitor.py" 
+  print("python plotCaMonitor.py [<filename>]")
+  print("example: python plotCaMonitor.py xx.txt")
+  print("example stdin: cat data.log | grep -E 'thread|CPU' | python plotCaMonitor.py")
 
 def main():
   # Check args
   if len(sys.argv)>1:
-    print sys.argv[1] 
+    print (sys.argv[1])
     pos1=sys.argv[1].find('-h')
     if(pos1>=0):
       printOutHelp()
       sys.exit()
-  
-  if len(sys.argv)!=2 and len(sys.argv)>1:  
-      printOutHelp()
-      sys.exit()
-  
-  if len(sys.argv)==2:
-    fname=sys.argv[1]
-    dataFile=open(fname,'r')
-
-  if len(sys.argv)==1:
+    pos1=sys.argv[1].find('-t')
+    if(pos1>=0) and len(sys.argv)>2:
+      title = sys.argv[2]
+      if len(sys.argv)>3:
+        fname=sys.argv[3]
+        dataFile=open(fname,'r')
+      else:
+        fname=""      
+        dataFile=sys.stdin
+    else:
+      title=""
+      fname=sys.argv[2]
+      dataFile=open(fname,'r')
+  else:
+    title=""
     fname=""
-    dataFile=sys.stdin;
+    dataFile=sys.stdin
 
   parser=caMonitorArrayParser()
   pvs=[]
@@ -54,35 +59,43 @@ def main():
     pvToAddDataTo.setValues(timeVal,data)
     if newPv:       
       pvs.append(pvToAddDataTo)
-      print "Added PV: " + pvName
+      print("Added PV: " + pvName)
   
-  print  "Statistics: "  
+  print("Statistics: ")
   legend=[]
+  count=0
   for pv in pvs: 
     
-    
+    count+=1
     timeSet, dataSet=pv.getData()     
     #for d in dataSet:
-    #  print d
+    #  print(d)
     pvLength = pv.getLength()    
     pvMax = np.max(dataSet)
     pvMin = np.min(dataSet)
     pvAvg = np.mean(dataSet)
     pvStd = np.std(dataSet)
-    legStr = pv.getName() + "[" + str(pvLength) + "] " + str(pvMin) + ".." + str(pvMax) + ", mean: " + str(pvAvg) + ", std: " + str(pvStd)
+    legStr = pv.getName() + "[" + str(pvLength) + "] " + str(pvMin) + ".." + str(pvMax) + ", mean: " + str(pvAvg) + ", std: " + str(pvStd) + ", range: " +str(pvMax-pvMin)
+    #infoStr = "[" + str(pvLength) + "] " + str(pvMin) + ".." + str(pvMax) + ", mean: " + str(pvAvg) + ", std: " + str(pvStd) + ", range: " +str(pvMax-pvMin)
+    infoStr = pv.getName() + "[{0}]:\n  range: {1:.7f}.. {2:.7f} ({3:.7f}), \n  mean: {4:.7f},\n  std: {5:.7f}".format(pvLength, pvMin,pvMax,pvMax-pvMin,pvAvg,pvStd)
     legend.append(pv.getName())
      
     x=timeSet
     
-    print legStr
-    n, bins, patches = plt.hist(dataSet, 50, normed=1)
+    print(legStr)
+    plt.figure(figsize=(8, 8))
+    n, bins, patches = plt.hist(dataSet, 50, density=1)
     y = mlab.normpdf( bins, pvAvg, pvStd)
-    l = plt.plot(bins, y, linewidth=1)    
+    l = plt.plot(bins, y, linewidth=1) 
+
+  if count == 1:    
+    plt.gcf().text(0.01,0.91,infoStr) 
 
   plt.legend(legend)
   plt.grid()
-  plt.title(fname)
+  plt.title(title)
   plt.show()
-  
+
+
 if __name__ == "__main__":
   main()
