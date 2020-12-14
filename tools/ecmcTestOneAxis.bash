24fcc07718d62b3f46e043cb522636c05c276fa2
Anders Sandström <anders.sandstrom@esss.se>
2020-12-14 10:04:04 +0100

    WIP

--- tools/ecmcTestOneAxis.bash
+++ tools/ecmcTestOneAxis.bash
@@ -3,6 +3,7 @@
 # 
 motorPV="IOC_TEST:Axis1"
 testPV="IOC_TEST:TestNumber"
+velo=0.75
 
 echo "Starting ecmc automatic slit SAT script!"
 
@@ -21,37 +22,46 @@ fi
 echo "0.1 Homing with sequence 1 (to low limit)..."
 python ecmcTestHome.py ${motorPV} ${testPV} 1 10
 
-echo "1. Starting resolver standstill jitter test.."
-python ecmcTestResolver.py ${motorPV} ${testPV} 0.125 8 0.75 1000
-
 echo "2. Starting low limit jitter test.."
-python ecmcTestLimitBwd.py ${motorPV} ${testPV} 2 0.75 2000
+python ecmcTestLimitBwd.py ${motorPV} ${testPV} 2 $velo 2000
 
 echo "3.1 Starting repeatability test pos 15 (from below).."
-python ecmcTestRepeatability.py ${motorPV} ${testPV} 13 15 0.75 3100
+python ecmcTestRepeatability.py ${motorPV} ${testPV} 13 15 $velo 3100
 
 echo "3.2 Starting repeatability test pos 15 (from above).."
-python ecmcTestRepeatability.py ${motorPV} ${testPV} 17 15 0.75 3200
+python ecmcTestRepeatability.py ${motorPV} ${testPV} 17 15 $velo 3200
 
 echo "3.3 Starting repeatability test pos 35 (from below).."
-python ecmcTestRepeatability.py ${motorPV} ${testPV} 33 35 0.75 3300
+python ecmcTestRepeatability.py ${motorPV} ${testPV} 33 35 $velo 3300
 
 echo "3.4 Starting repeatability test pos 35 (from above).."
-python ecmcTestRepeatability.py ${motorPV} ${testPV} 37 35 0.75 3400
+python ecmcTestRepeatability.py ${motorPV} ${testPV} 37 35 $velo 3400
+
+echo "1. Starting resolver standstill jitter test.."
+python ecmcTestResolver.py ${motorPV} ${testPV} 0.125 8 $velo 1000
 
 echo "3.5 Starting repeatability test pos 55 (from below).."
-python ecmcTestRepeatability.py ${motorPV} ${testPV} 53 55 0.75 3500
+python ecmcTestRepeatability.py ${motorPV} ${testPV} 53 55 $velo 3500
 
 echo "3.6 Starting repeatability test pos 55 (from above).."
-python ecmcTestRepeatability.py ${motorPV} ${testPV} 57 55 0.75 3600
+python ecmcTestRepeatability.py ${motorPV} ${testPV} 57 55 $velo 3600
 
 echo "4. Starting high limit jitter test.."
-python ecmcTestLimitFwd.py ${motorPV} ${testPV} 2 0.75 4000
+python ecmcTestLimitFwd.py ${motorPV} ${testPV} 2 $velo 4000
 
 echo "5. Scan backward to low limit.."
-python ecmcTestScanBwd.py ${motorPV} ${testPV} 0.75 200 5000
+python ecmcTestScanBwd.py ${motorPV} ${testPV} $velo 200 5000
 
-echo "6. Scan forward to hig limit.."
-python ecmcTestScanFwd.py ${motorPV} ${testPV} 0.75 200 6000
+echo "6. Scan forward to high limit.."
+python ecmcTestScanFwd.py ${motorPV} ${testPV} $velo 200 6000
+
+echo "7. Step backward to low limit.."
+python ecmcTestStepScan.py ${motorPV} ${testPV} 60 5 5 $velo 7000
+
+echo "x.x Move to pos 3 to get correct dir to approach pos 5 in test 8.."
+python ecmcTestMovePos.py ${motorPV} 3 $velo
+
+echo "8. Step forward to high limit.."
+python ecmcTestStepScan.py ${motorPV} ${testPV} 5 60 5 $velo 8000
 
 echo "Tests finalized, please stop data acquisition.."
---
+++ tools/ecmcTestStepScan.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import epics
+import os
+import sys
+import time
+import math
+import unittest
+import ecmcSlitDemoLib
+
+homedPvSuffix='-homed'
+testLoops = 10
+
+if len(sys.argv)!=8:
+  print("python ecmcTestStepScan.py <motorPvNamepv> <testnumberpv> <startpos> <stoppos> <step> <velo> <testbasenum>")
+  print("python ecmcTestStepScan.py IOC:Axis1 IOC:TestNumber 60 5 5 0.75 7100")
+  sys.exit()
+
+
+motorPvName = sys.argv[1]
+testPvname  = sys.argv[2]
+motorHomedPvName = motorPvName + homedPvSuffix
+startpos = float(sys.argv[3])
+stoppos = float(sys.argv[4])
+stepsize = float(sys.argv[5])
+velo  = float(sys.argv[6])
+testNumberBase = float(sys.argv[7])
+homedPv = epics.PV(motorHomedPvName)
+testPv = epics.PV(testPvname)
+
+if homedPv is None:
+   print ("Invalid homed pv") 
+   sys.exit()
+
+if testPv is None:
+   print ("Invalid testPv") 
+   sys.exit()
+   
+homed = homedPv.get()
+if not homed:
+   print ("Motor not homed. Test will abort.")
+   sys.exit()
+
+if stepsize<=0:
+   print ("Invalid stepsize.")
+   sys.exit()
+
+testPv.put(testNumberBase)
+
+print ('Disable amplifier')
+ecmcSlitDemoLib.setAxisEnable(motorPvName, 0)
+time.sleep(0.2) #ensure that enabled goes down
+error=ecmcSlitDemoLib.getAxisError(motorPvName,1)
+
+#Reset error on all axis
+print ('Reset error axes.')
+ecmcSlitDemoLib.setAxisReset(motorPvName, 1)
+time.sleep(0.2)
+ecmcSlitDemoLib.setAxisReset(motorPvName, 0)
+
+print ('Enable amplifier')
+ecmcSlitDemoLib.setAxisEnable(motorPvName, 1)
+time.sleep(0.2) #ensure that enabled goes down
+error=ecmcSlitDemoLib.getAxisError(motorPvName,1)
+counter = 0
+
+timeOut = 50
+
+pos = startpos
+counter=0
+done = False
+
+if startpos>stoppos:
+    done = pos < stoppos
+
+if startpos<stoppos:
+    done = pos > stoppos
+
+while not done:
+  print ('Move axis to position ' + str(pos))
+  done=ecmcSlitDemoLib.moveAxisPosition(motorPvName,pos,velo,timeOut)
+  if not done:
+    print (motorPvName + " failed to position.")
+    sys.exit()
+  timeOut = stepsize / velo * 2
+  counter = counter + 1
+  time.sleep(1)
+  testPv.put(testNumberBase+counter)
+
+  if startpos>stoppos:
+    pos = pos - stepsize
+    done = pos < stoppos
+  if startpos<stoppos:    
+    pos = pos + stepsize
+    done = pos > stoppos
+
+time.sleep(1)
+testPv.put(testNumberBase)
+print("Test done!")
